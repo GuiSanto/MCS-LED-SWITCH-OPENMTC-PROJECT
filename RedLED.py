a1106f83ddb7ed58434cee7f0b2af595cd78278b
GuiSanto <99729373+GuiSanto@users.noreply.github.com>
2022-04-19 15:49:05 +0100

    1 Switch 2 LEDs done, no MTC

---
+++ RedLED.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+from tkinter import *
+ 
+#Set display sizes
+WINDOW_W = 1920
+WINDOW_H = 1080
+
+#Set display sizes
+BUTTON_SIZE = 1920
+MARGIN = 2
+
+#Set colours
+BLACK = '#000000'
+RED = '#ff0000'
+GREY = '#808080'
+
+
+LIGHTOFFON=[GREY,RED]
+OFF = 0
+ON = 1
+colourBackground = BLACK
+colourButton = GREY
+#Light Status
+lightStatus=[OFF]
+
+def createDisplay():
+  global tk, canvas, light
+  # create the tk window - within which
+  # everything else will be built.
+  tk = Tk()
+  #Add a canvas area ready for drawing on
+  canvas = Canvas(tk, width=WINDOW_W, height=WINDOW_H, background=BLACK)
+  canvas.pack()
+  #Add some "lights" to the canvas
+  light = []
+  x = MARGIN+((MARGIN+BUTTON_SIZE)*0)
+  light.append(canvas.create_rectangle(x,MARGIN,
+  x+BUTTON_SIZE,BUTTON_SIZE+MARGIN,fill=GREY))
+  
+  # Start the tk main-loop (this updates the tk display)
+  
+  # Create keyboard bindings
+  tk.bind_all('<KeyPress-1>', handleInput)
+  
+  tk.mainloop()
+ 
+def terminate():
+ global tk
+ tk.destroy()
+ 
+ 
+def handleInput(event):
+  inputkey = 0
+  if event.keysym == '1':
+    inputkey = 1
+  #Handle the keyboard input
+  if inputkey != 0:
+    lightStatus[inputkey-1] = toggle(lightStatus[inputkey-1])
+    canvas.itemconfig(light[inputkey-1], fill=LIGHTOFFON[lightStatus[inputkey-1]])
+    
+def toggle(value):
+  if value == ON:
+    value = OFF
+  else:
+    value = ON
+  return value
+ 
+def main():
+ createDisplay()
+ 
+if __name__ == '__main__':
+ main()
---
+++ Switch.py
@@ -0,0 +1,38 @@
+from tkinter import *
+import tkinter.font as font
+
+def select_led():
+ if(button1['text'] == 'LED 1'):
+  button1['text'] = 'LED 2'
+ else:
+  button1['text'] = 'LED 1'
+
+
+def select_state():
+ if(button2['text'] == 'ON'):
+  button2['text'] = 'OFF'
+ else:
+  button2['text'] = 'ON'
+	
+	
+window=Tk()
+
+#title
+label=Label(window, text="Virtual Switch", fg='red', font=("Helvetica", 40))
+label.place(x=800, y=50)
+
+#text size of both buttons
+f = font.Font(family='Times New Roman', size=50, weight="bold")
+
+#button to select the LED
+button1=Button(window, fg = 'black', text = "LED 1", font = f, command = select_led, height = 9, width= 23)
+button1.place(x=100, y=200)
+
+#button to turn ON or OFF
+button2=Button(window, fg = 'black', text = "OFF", font = f, command = select_state, height = 9, width= 23)
+button2.place(x=950, y=200)
+
+window.title('Virtual Switch')
+window.geometry("1920x1080")
+
+window.mainloop()
